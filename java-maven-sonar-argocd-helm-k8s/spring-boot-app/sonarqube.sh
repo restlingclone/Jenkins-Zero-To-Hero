cc182ffe7becc74895b0596d0ad3ecf824604561
Shubham Sanjay Age <46015549+restlingclone@users.noreply.github.com>
2023-12-20 16:02:47 +0530

    Final updated of the script

--- java-maven-sonar-argocd-helm-k8s/spring-boot-app/sonarqube.sh
+++ java-maven-sonar-argocd-helm-k8s/spring-boot-app/sonarqube.sh
@@ -1,5 +1,3 @@
-#!/bin/bash
-
 # Install Sonarqube server setup on ubuntu 22 and latest.
 #Update the server and install dependencies package.
 sudo DEBIAN_FRONTEND=noninteractive apt update -y;
@@ -7,24 +5,21 @@ sudo DEBIAN_FRONTEND=noninteractive apt install unzip -y;
 
 # Add sonarqube user and install sonarqube with latest community version.
 # Define username as sonarqube
-sudo username=sonarqube
-sudo password=admin
+username=sonarqube
+password=admin
 # Now create the user with password
-sudo adduser $username --disabled-password --gecos Sonarqube;
-sudo echo "$username:$password" | chpasswd ;
-sudo echo Sonarqube: $username has been created with password: $password ;
+sudo adduser $username --disabled-password --gecos Sonarqube
+echo "$username:$password" | sudo chpasswd
+echo Sonarqube: $username has been created with password: $password
 # Sonarqube: sonarqube has been created with password: admin
-sudo su - sonarqube ;
-
+sudo su - sonarqube
 #Install sonarqube and start the setup.
-sudo wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.3.79811.zip ;
-sudo unzip * ;
-sudo chmod -R 755 /home/sonarqube/sonarqube-9.4.0.54424 ; 
-sudo chown -R sonarqube:sonarqube /home/sonarqube/sonarqube-9.4.0.54424 ; 
-sudo cd sonarqube-9.4.0.54424/bin/linux-x86-64/ ; 
-sudo ./sonar.sh start
+wget https://binaries.sonarsource.com/Distribution/sonarqube/sonarqube-9.9.3.79811.zip
+unzip * 
+chmod -R 755 /home/sonarqube/sonarqube-9.9.3.79811
+chown -R sonarqube:sonarqube /home/sonarqube/sonarqube-9.9.3.79811 
+cd sonarqube-9.9.3.79811/bin/linux-x86-64
+ ./sonar.sh start
 
 # Sonarqube steup is done, login onto your browser with URL http://<public-ip-address>:9000 and allow 9000 port in your security group.
 # Username and password is admin to first time login.
-
-sync; echo 3 > /proc/sys/vm/drop_caches 
